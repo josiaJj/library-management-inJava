c60f91cbf59777ae1a6475580b6e4387fb2bec3c
josiaJj <hei.josia@gmail.com>
2023-11-19 08:16:07 +0300

    Implement Topic class with fields, getters, setters, and toString() method

--- src/main/java/school/hei/Topic.java
+++ src/main/java/school/hei/Topic.java
@@ -1,4 +1,30 @@
 package school.hei;
 
 public class Topic {
+    public Topic(String topicName) {
+        this.topicName = topicName;
+    }
+    private Integer id;
+    private String topicName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTopicName() {
+        return topicName;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTopicName(String topicName) {
+        this.topicName = topicName;
+    }
+
+    @Override
+    public String toString() {
+        return topicName;
+    }
 }
