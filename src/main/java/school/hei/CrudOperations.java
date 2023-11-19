2062428e014ce0e6bd2aec8f02d714c2c7bbc5b2
josiaJj <hei.josia@gmail.com>
2023-11-19 08:32:41 +0300

    Introduce CrudOperations interface for generic CRUD operations

---
+++ src/main/java/school/hei/CrudOperations.java
@@ -0,0 +1,13 @@
+package school.hei;
+
+import java.util.List;
+
+public interface CrudOperations<T> {
+    List<T> findAll();
+
+    List<T> saveAll(List<T> toSave);
+
+    T save(T toSave);
+
+    T delete(T toDelete);
+}
