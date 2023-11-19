84fd1c3475c58accc061fd6098095a510cfb334c
josiaJj <hei.josia@gmail.com>
2023-11-19 08:09:07 +0300

    Add default constructor and override toString() method in Subscribers class

--- src/main/java/school/hei/Subscribers.java
+++ src/main/java/school/hei/Subscribers.java
@@ -1,4 +1,12 @@
 package school.hei;
 
-public class Subscribers extends User{
+public class Subscribers extends User {
+    public Subscribers() {}
+    @Override
+    public String toString() {
+        return "Username= " + username +
+                ", Password= " + password +
+                ", Sex= " + sex +
+                "} \n";
+    }
 }
