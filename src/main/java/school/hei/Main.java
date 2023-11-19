589cd80ad6d6a83bf2997e75a9791b824cb0dea8
josiaJj <hei.josia@gmail.com>
2023-11-19 19:22:28 +0300

    Implement findAll() method to AuthorCrudOperation

--- src/main/java/school/hei/Main.java
+++ src/main/java/school/hei/Main.java
@@ -1,7 +1,24 @@
 package school.hei;
 
+import school.hei.Model.Author;
+import school.hei.Model.Book;
+import school.hei.Repository.AuthorCrudOperations;
+import school.hei.Repository.BookCrudOperations;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Book book = new Book();
+        Author author = new Author();
+
+        BookCrudOperations bookCrudOperations = new BookCrudOperations();
+        AuthorCrudOperations authorCrudOperations = new AuthorCrudOperations();
+
+        List<Book> allBooks = bookCrudOperations.findAll();
+        List<Author> allAuthors = authorCrudOperations.findAll();
+
+        System.out.println("List of all Books : \n" + allBooks);
+        System.out.println("List of all Authors : \n" + allAuthors);
     }
 }
