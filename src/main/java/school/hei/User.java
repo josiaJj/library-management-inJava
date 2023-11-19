594744b0fb7eff164735dd8e13020f33f09e2a14
josiaJj <hei.josia@gmail.com>
2023-11-19 08:04:24 +0300

    Add getters and setters for username, password, and sex in User class

--- src/main/java/school/hei/User.java
+++ src/main/java/school/hei/User.java
@@ -1,4 +1,31 @@
 package school.hei;
 
 public class User {
+    String username;
+    String password;
+    Sex sex;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
 }
