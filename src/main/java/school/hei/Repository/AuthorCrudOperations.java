3b8f82e5ae81f5c89df232e458b1b5a7a62c63bf
josiaJj <hei.josia@gmail.com>
2023-11-19 22:03:43 +0300

    Implement methods saveAll(List<T> toSave), save(T toSave), delete(T toDelete) in AuthorCrudOperations

--- src/main/java/school/hei/Repository/AuthorCrudOperations.java
+++ src/main/java/school/hei/Repository/AuthorCrudOperations.java
@@ -4,10 +4,7 @@ import school.hei.Model.Author;
 import school.hei.Model.Book;
 import school.hei.Model.Sex;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,17 +39,90 @@ public class AuthorCrudOperations implements CrudOperations<Author> {
 
     @Override
     public List<Author> saveAll(List<Author> toSave) {
-        return null;
+        String insertAuthorQuery = "INSERT INTO author (author_name, sex) VALUES (?, ?)";
+
+        try (Connection connection = DB_Connection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(insertAuthorQuery, Statement.RETURN_GENERATED_KEYS)) {
+
+            List<Author> savedAuthors = new ArrayList<>();
+
+            for (Author author : toSave) {
+                preparedStatement.setString(1, author.getAuthorName());
+                preparedStatement.setString(2, author.getSex().toString());
+
+                preparedStatement.addBatch();
+            }
+
+            int[] affectedRows = preparedStatement.executeBatch();
+
+            try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+                int i = 0;
+                while (generatedKeys.next()) {
+                    Author savedAuthor = new Author();
+                    savedAuthor.setId(generatedKeys.getLong(1));
+                    savedAuthors.add(savedAuthor);
+                    i++;
+                }
+            }
+
+            return savedAuthors;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error saving authors", e);
+        }
     }
 
     @Override
     public Author save(Author toSave) {
-        return null;
+        String insertAuthorQuery = "INSERT INTO author (author_name, sex) VALUES (?, ?)";
+
+        try (Connection connection = DB_Connection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(insertAuthorQuery, Statement.RETURN_GENERATED_KEYS)) {
+
+            preparedStatement.setString(1, toSave.getAuthorName());
+            preparedStatement.setString(2, toSave.getSex().toString());
+
+            int affectedRows = preparedStatement.executeUpdate();
+
+            if (affectedRows == 0) {
+                throw new SQLException("Creating author failed, no rows affected.");
+            }
+
+            try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    toSave.setId(generatedKeys.getLong(1)); // Assuming the ID is the first column
+                } else {
+                    throw new SQLException("Creating author failed, no ID obtained.");
+                }
+            }
+
+            return toSave;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error saving author", e);
+        }
     }
 
     @Override
     public Author delete(Author toDelete) {
-        return null;
+        String deleteAuthorQuery = "DELETE FROM author WHERE id = ?";
+
+        try (Connection connection = DB_Connection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(deleteAuthorQuery)) {
+
+            preparedStatement.setLong(1, toDelete.getId());
+
+            int affectedRows = preparedStatement.executeUpdate();
+
+            if (affectedRows == 0) {
+                throw new SQLException("Deleting author failed, no rows affected.");
+            }
+
+            return toDelete;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error deleting author", e);
+        }
     }
 
 
