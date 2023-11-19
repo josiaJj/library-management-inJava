c219a69c2f3b3c45799848a1b6ed168a66479044
josiaJj <hei.josia@gmail.com>
2023-11-19 08:11:41 +0300

    Enhance Author class with fields, getters, setters, and toString() method

--- src/main/java/school/hei/Author.java
+++ src/main/java/school/hei/Author.java
@@ -1,4 +1,40 @@
 package school.hei;
 
 public class Author {
+    public Author() {}
+    private Long id;
+    private String authorName;
+    private Sex sex;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "id= " + id +
+                ", authorName= " + authorName +
+                ", Sex= " + sex +
+                "} \n";
+    }
 }
