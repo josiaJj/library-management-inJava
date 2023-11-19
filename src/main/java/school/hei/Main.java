caf8e159e89f9101c46a07cb25b79d0b65d78d5e
josiaJj <hei.josia@gmail.com>
2023-11-19 19:26:12 +0300

    Implement findAll() method to SubscribersCrudOperations

--- src/main/java/school/hei/Main.java
+++ src/main/java/school/hei/Main.java
@@ -2,8 +2,10 @@ package school.hei;
 
 import school.hei.Model.Author;
 import school.hei.Model.Book;
+import school.hei.Model.Subscribers;
 import school.hei.Repository.AuthorCrudOperations;
 import school.hei.Repository.BookCrudOperations;
+import school.hei.Repository.SubscribersCrudOperations;
 
 import java.util.List;
 
@@ -11,14 +13,18 @@ public class Main {
     public static void main(String[] args) {
         Book book = new Book();
         Author author = new Author();
+        Subscribers subscribers = new Subscribers();
 
         BookCrudOperations bookCrudOperations = new BookCrudOperations();
         AuthorCrudOperations authorCrudOperations = new AuthorCrudOperations();
+        SubscribersCrudOperations subscribersCrudOperations = new SubscribersCrudOperations();
 
         List<Book> allBooks = bookCrudOperations.findAll();
         List<Author> allAuthors = authorCrudOperations.findAll();
+        List<Subscribers> allSubscribers = subscribersCrudOperations.findAll();
 
         System.out.println("List of all Books : \n" + allBooks);
         System.out.println("List of all Authors : \n" + allAuthors);
+        System.out.println("List of all Subscribers : \n" + allSubscribers);
     }
 }
--- src/main/java/school/hei/Repository/SubscribersCrudOperations.java
+++ src/main/java/school/hei/Repository/SubscribersCrudOperations.java
@@ -1,5 +1,8 @@
 package school.hei.Repository;
 
+import school.hei.Model.Sex;
+import school.hei.Model.Subscribers;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -7,6 +10,47 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SubscribersCrudOperations {
+public class SubscribersCrudOperations implements CrudOperations<Subscribers> {
+    @Override
+    public List<Subscribers> findAll() {
+        List<Subscribers> subscribersList = new ArrayList<>();
+
+        String findAllSubscribers_request = "SELECT * FROM subscribers";
+
+        try (
+                Connection connection = DB_Connection.getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(findAllSubscribers_request)
+        ) {
+            while(resultSet.next()) {
+                Subscribers subscribers = new Subscribers();
+
+                subscribers.setUsername(resultSet.getString("username"));
+                subscribers.setPassword(resultSet.getString("password"));
+                subscribers.setSex(Sex.valueOf(resultSet.getString("sex")));
+
+                subscribersList.add(subscribers);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return subscribersList;
+    }
+
+    @Override
+    public List<Subscribers> saveAll(List<Subscribers> toSave) {
+        return null;
+    }
+
+    @Override
+    public Subscribers save(Subscribers toSave) {
+        return null;
+    }
+
+    @Override
+    public Subscribers delete(Subscribers toDelete) {
+        return null;
+    }
+
 
 }
